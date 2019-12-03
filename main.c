e033c5ae0b77998eea41b55da387fce3d651711d
jjan0221 <jjan0221@sookmyung.ac.kr>
2019-12-03 15:44:29 +0900

    2

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "keyword.h"
 
 int is_whitespace(char c)
 {
@@ -68,11 +69,11 @@ int main(int argc, char *argv[]) {
 	//word reading & analysis
 	while(fget_word(fp,word) !=0)
 	{
-		printf("%s\n", word);
+		count_word(word);
 	}
 	
 	//output
-	
+	print_word();
 	
 	fclose(fp);
 	
